aae79006d24859a5a4c781422070375c21d9f2c3
arju10 <arjuarju71@gmail.com>
2025-07-14 11:14:32 +0600

    Project Setup with Eslint Prrettier formatting

--- paw-connect-server/src/server.ts
+++ paw-connect-server/src/server.ts
@@ -1,21 +1,13 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import app from './app';
+import { Server } from 'http';
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Sample route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Paw Connect Server!');
-});
-
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+async function main() {
+  const server: Server = app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
 
-export default app;
+main();
