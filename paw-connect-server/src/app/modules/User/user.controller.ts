4cd92de25880ff3c6855a4e004a661a2fca6b47f
arju10 <arjuarju71@gmail.com>
2025-07-21 16:01:57 +0600

    Response Formating and Exception Handling

--- paw-connect-server/src/app/modules/User/user.controller.ts
+++ paw-connect-server/src/app/modules/User/user.controller.ts
@@ -5,8 +5,20 @@ import { userServices } from './user.service';
 const createAdmin = async (req: Request, res: Response) => {
   //   console.log('User Controllers!');
   // console.log(req.body)
-  const result = await userServices.createAdminIntoDatabase(req.body);
-  res.send(result);
+  try {
+    const result = await userServices.createAdminIntoDatabase(req.body);
+    res.status(200).json({
+      success: true,
+      message: 'Admin created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error?.message || 'Something went wrong',
+      error: error,
+    });
+  }
 };
 
 export const userController = {
