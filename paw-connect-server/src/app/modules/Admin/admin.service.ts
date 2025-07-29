6e09eb1f85e2815d1ff4e66ac0dbdba8572ed894
arju10 <arjuarju71@gmail.com>
2025-07-29 16:51:24 +0600

    addConditions apply

--- paw-connect-server/src/app/modules/Admin/admin.service.ts
+++ paw-connect-server/src/app/modules/Admin/admin.service.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { constants } from 'buffer';
 
 const prisma = new PrismaClient();
 
 const getAllAdminFromDB = async (params: any) => {
   // console.log(params);
-  const addConditions = [];
+  const addConditions: Prisma.AdminWhereInput[] = [];
   if (params.searchTerm) {
     addConditions.push({
       OR: [
@@ -17,20 +17,20 @@ const getAllAdminFromDB = async (params: any) => {
         },
         {
           email: {
-            constants: params.searchTerm,
+            contains: params.searchTerm,
             mode: 'insensitive',
           },
         },
       ],
     });
   }
-  console.log(addConditions,{depth:'infinity'})
+  // console.log(addConditions, { depth: 'infinity' });
 
-
-  // const result = await prisma.admin.findMany({
-  //   where:{}
-  // });
-  // return result;
+  const whereConditions: Prisma.AdminWhereInput = { AND: addConditions };
+  const result = await prisma.admin.findMany({
+    where: whereConditions,
+  });
+  return result;
 };
 
 export const AdminService = {
